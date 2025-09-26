54fcc158ff97b3296920e7ca45821da3ef01c42b
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-27 00:28:37 +0100

    Create bash.sh

---
+++ var/www/repo/bash.sh
@@ -0,0 +1,10 @@
+```bash
+   /var/www/repo/
+   ├── stable/
+   │     ├── hello-world-1.0.0.aura
+   │     ├── editor-0.9.4.aura
+   │     └── index.json
+   ├── testing/
+   ├── nightly/
+   └── keys/
+         └── aura-public.gpg
