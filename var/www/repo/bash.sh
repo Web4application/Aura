507bf74f734cc4947a5155780a58660e02075a66
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-27 00:30:17 +0100

    Update bash.sh

--- var/www/repo/bash.sh
+++ var/www/repo/bash.sh
@@ -14,3 +14,19 @@ scp myapp-2.0.0.aura repo@auraos.org:/var/www/repo/stable/
 
 # Update repo index
 ssh repo@auraos.org "apm-repo"
+
+# Generate Aura signing key
+gpg --quick-gen-key "AuraOS Repo <repo@auraos.org>" rsa4096 sign 1y
+
+# Export public key for clients
+gpg --armor --export "AuraOS Repo" > /var/www/repo/keys/aura-public.gpg
+
+# Import repo public key (once)
+gpg --import aura-public.gpg
+
+# Install package
+apm install hello-world
+
+# apm verifies:
+#   - SHA256 hash from index.json
+#   - GPG signature on package + index
