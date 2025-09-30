e25494bde5b9fa7b12d376481124b2d3cc14f0ab
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-30 01:24:28 +0100

    Update and rename .ps1 to Wsl/aura/install.sh

--- Wsl/aura/install.sh
+++ Wsl/aura/install.sh
@@ -1,11 +1,10 @@
 # Let's go in your home directory and prepare the working directories
-cd ~
-mkdir -p WSL\YunoHost
+cd ~mkdir -p WSL\Aura
 # Download the Debian appx package and unzip it
 curl.exe -L -o debian.zip https://aka.ms/wsl-debian-gnulinux
 Expand-Archive .\debian.zip -DestinationPath .\debian
 # Import the Debian base into a new distro
-wsl --import YunoHost ~\WSL\YunoHost ~\debian\install.tar.gz --version 2
+wsl --import Aura ~\WSL\Aura ~\debian\install.tar.gz --version 2
 # Cleanup
 rmdir .\debian -R
 
@@ -36,5 +35,4 @@ sudo su
 genie -s
 # Your hostname should have been appended with "-wsl"
 
-cd ~
-wsl --export YunoHost .\WSL\YunoHost.tar.gz
+cd ~wsl --export Aura .\WSL\YunoHost.tar.gz
